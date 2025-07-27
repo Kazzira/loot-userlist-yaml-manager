a3dff17a40057b3bd14f8c343c9862e6c8fb808c
Zachary Dakota Meyer <dev.zdmeyer@gmail.com>
2025-07-26 19:37:24 -0500

    Formatting

--- include/luyamlman/error/details_types/allocation_failure.hpp
+++ include/luyamlman/error/details_types/allocation_failure.hpp
@@ -4,10 +4,10 @@ Copyright (C) 2024-2025 Zachary Dakota Meyer. All rights reserved.
 
 This file is part of the LOOT Userlist.yaml Manager project.
 
-LOOT Userlist.yaml Manager is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+LOOT Userlist.yaml Manager is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your option)
+any later version.
 
 LOOT Userlist.yaml Manager is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,15 +15,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with LOOT Userlist.yaml Manager.  If not, see <http://www.gnu.org/licenses/>.
+along with LOOT Userlist.yaml Manager.  If not, see
+<http://www.gnu.org/licenses/>.
 */
 
 namespace luyamlman::error_details_types {
 
-//PAGE: struct allocation_failure_error_details -----------------------------/
-struct t_allocation_failure {};
+struct t_allocation_failure
+{
+};
 
-
-}
-
-//PAGE: EOF -----------------------------------------------------------------/
+} // namespace luyamlman::error_details_types
